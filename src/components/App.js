3334320b3ddc29a6aa1384dc78a7c96bbdc2e45d
Mr-R-e-X <souvikhazra151@gmail.com>
2024-06-27 01:58:07 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
 import React from "react";
-import './../styles/App.css';
+import { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app-container">
+      {isLoggedIn ? (
+        <h1 className="logged-in-heading">You are logged in!</h1>
+      ) : (
+        <div className="form-container">
+          <form className="login-form">
+            <input type="text" placeholder="username" className="form-input" />
+            <input
+              type="password"
+              placeholder="password"
+              className="form-input"
+            />
+            <button onClick={() => setLoggedIn(true)} className="form-button">
+              Login
+            </button>
+          </form>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
